5382b8ad072b22feeb9a4b807647ddc43f86840b
Kim Söderlund <kimsoderlund33@gmail.com>
2025-11-20 09:51:14 +0100

    Added, Keyboard Eventlisteners & Styling to for-loop

--- script.js
+++ script.js
@@ -18,8 +18,11 @@ document.getElementById("infoReturnBtn").addEventListener('click', function () {
     document.getElementById("infOverlay").classList.add("hideScreen")
 });
 
-//To do: Flytta kod här nedan vidare till en egen JS fil där Game-view koden ligger?
 
+
+
+
+//To do: Flytta kod här nedan vidare till en egen JS fil där Game-view koden ligger?
 //tangentbordets variabler
 const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","Å","Ä","Ö"];
 const keyboardDiv = document.getElementById("keyboard");
@@ -29,17 +32,12 @@ const keyboardKeys = document.querySelectorAll('#gameScreen .keyboard p');
 for (let i = 0; i < letters.length; i++) {
     const p = document.createElement("p");
     p.textContent = letters[i];
+
+    //lägg styling och listener på alla knappar. Lägg ytterligare funktionalitet under eventlistener?
+    p.classList.add("keyboardBtn");
+    p.addEventListener('click', function() {
+    p.classList.add("activeKey");
+  });
+
     keyboardDiv.appendChild(p);
   }
-
-//Ge alla knappar samma styling
-keyboardKeys.forEach(keyboardKeys => {
-    keyboardKeys.classList.add("keyboardBtn");
-
-    //Klickar man på en knapp så blir den aktiv, Dvs får klassen activeKey.
-    //To do: Lägga till all funktionalitet här.
-    keyboardKeys.addEventListener('click', function() { 
-        keyboardKeys.classList.add("activeKey");
-    }
-    );
-});
