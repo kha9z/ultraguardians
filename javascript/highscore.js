2ff2e3cc6e42133f5c08f27cbc4ac313e1730a9d
Kim Söderlund <kimsoderlund33@gmail.com>
2025-11-24 11:08:13 +0100

    Fixed grafic bugs with game over screen button

--- javascript/highscore.js
+++ javascript/highscore.js
@@ -2,15 +2,13 @@ const gameOverButton = document.getElementById('gameOverButton');
 const playerNameInput = document.getElementById('playerName');
 const KEY = 'highscores';
 
-//Ändra knapp beroende på om spelaren skrivit något eller inte
+// Ändra knapp beroende på om spelaren skrivit något eller inte
 playerNameInput.addEventListener('input', () => {
-    if (playerNameInput.trim() === '') {
-        gameOverButton.textContent = "Återgå till Start"
-    } else { gameOverButton.textContent = "Återgå & Skicka"
-}
-
-//To do: Kanske ha kod för att hindra förändring av knappen då spelare inte har nog hög poäng för att komma med på toplistan? 
-
+    if (playerNameInput.vaue === '') {
+        gameOverButton.textContent = "Återgå till Start";
+    } else {
+        gameOverButton.textContent = "Återgå & Skicka";
+    }
 });
 
 //Klick på återgå knapp
